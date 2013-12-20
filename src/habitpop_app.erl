430fff2401148f48784ca869124d7d78b2d1c8ec
Tim Ruffles <timruffles@googlemail.com>
2013-12-20 21:36:24 +0000

    init

---
+++ src/habitpop_app.erl
@@ -0,0 +1,16 @@
+-module(habitpop_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    habitpop_sup:start_link().
+
+stop(_State) ->
+    ok.
