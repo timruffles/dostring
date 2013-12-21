35bd4cca341f29eaee9688ead854a44c6486d020
Tim Ruffles <timruffles@googlemail.com>
2013-12-21 15:30:48 +0000

    working first version - ugly thing where ebin requires a library directory above root

--- src/habitpop_app.erl
+++ src/habitpop_app.erl
@@ -10,7 +10,23 @@
 %% ===================================================================
 
 start(_StartType, _StartArgs) ->
+    io:format("hello from app"),
     habitpop_sup:start_link().
+
+% on tweet ->
+% existing user?
+%   existing habit?
+%     add
+%     check if they're due an event (milestone etc)
+%   else
+%     say hi
+% else
+%   intro message
+
+%% questions:
+% - do hashtags get parsed out already?
+% - do mentions?
+% - need to subscribe for @ mentions
 
 stop(_State) ->
     ok.
