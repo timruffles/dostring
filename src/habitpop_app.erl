9d189b311b18fcaf84b86973d2adf07d851c408d
Tim Ruffles <timruffles@googlemail.com>
2013-12-22 13:39:41 +0000

    new idea - drop twitter api stuff, do everything via redis queues

--- src/habitpop_app.erl
+++ src/habitpop_app.erl
@@ -10,8 +10,26 @@
 %% ===================================================================
 
 start(_StartType, _StartArgs) ->
-    io:format("hello from app"),
-    habitpop_sup:start_link().
+  inets:start(),
+  ssl:start(),
+  io:format("Ok let's get some tweets~n"),
+  spawn(fun go_tweet/0),
+  habitpop_sup:start_link().
+
+go_tweet() ->
+  Headers = {os:getenv("TW_C_KEY"), os:getenv("TW_C_SECRET"),os:getenv("TW_T_KEY"), os:getenv("TW_T_SECRET")},
+  {ok, Params} =  stream_client_util:keywords_to_track(["justin"]),
+  Over = stream_client:connect(stream_client_util:filter_url(), Headers, Params, fun(Data) ->
+    Tweet = proplists:get_value(<<"text">>, Data),
+    io:format("Erlang <3: ~s~n", [Tweet])
+  end),
+  case Over of
+    {ok, x} -> io:format("Over and out~p~n",[x]);
+    {error, x} -> io:format("error: ~p~n",[x])
+  end,
+  io:format("Done listening..~n").
+
+
 
 % on tweet ->
 % existing user?
@@ -30,3 +48,4 @@ start(_StartType, _StartArgs) ->
 
 stop(_State) ->
     ok.
+
