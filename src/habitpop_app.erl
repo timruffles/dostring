10ae560a12d466a6bd1032fc889e6c6964b77aab
Tim Ruffles <timruffles@googlemail.com>
2013-12-22 19:04:19 +0000

    mm v imperative solution, is it poss to get to [Tweets] -> [Events]?

--- src/habitpop_app.erl
+++ src/habitpop_app.erl
@@ -15,6 +15,7 @@
 
 start(_StartType, _StartArgs) ->
   {ok, Redis} = eredis:start_link(),
+  Events = spawn(handle_event),
   io:format("Ok let's get some tweets~n"),
   habitpop_sup:start_link().
 
@@ -23,7 +24,7 @@ on_tweet(Redis,Tweet,Notifier) ->
   {ok,Is} = eredis:q(Redis,["ISMEMBER","users",Tweet#tweet.username]),
   case Is of
       <<"0">> ->
-        Notifier ! {newuser,Tweet};
+        Notifier ! {newuser,[Tweet]};
       <<"1">> ->
         ok
   end,
@@ -31,14 +32,28 @@ on_tweet(Redis,Tweet,Notifier) ->
     {ok,Is} = eredis:q(Redis,["ISMEMBER",string:join([Tweet#tweet.username,"|","habits"]),Hashtag]),
     case Is of
       <<"0">> ->
-        {newhabit,Hashtag,Tweet};
+        Notifier ! {newhabit,[Hashtag,Tweet]};
       <<"1">> ->
-        {oldhabit,Hashtag,Tweet}
+        Notifier ! {oldhabit,[Hashtag,Tweet]}
     end
   end,
-  Events = list:filtermap(HabitEventsCb, Tweet#tweet.hashtags),
-  list:foreach(fun (Event) -> Notifier ! Event end,Events).
+  list:foreach(HabitEventsCb, Tweet#tweet.hashtags),
    
+new_user_message(Tweet) ->
+  io_lib:format("~s welcome! Tweet @habitadd when you perform your habit, and we'll track your progress. Try it now",Tweet#tweet.username).
+
+handle_event() ->
+  receive E of
+    {newuser,[T]} ->
+      handle_new_user(T),
+      handle_event();
+    {newhabit,[H,T]} ->
+      handle_new_habit(H,T),
+      handle_event();
+    {newhabit,[H,T]} ->
+      handle_old_habit(H,T),
+      handle_event()
+  end.
 
 on_tweet_test() ->
   false = true.
